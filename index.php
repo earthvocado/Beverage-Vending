0589d57142edc7c7dcb8c8370a1b9eb24b3a4e2f
earthvocado <147298914+earthvocado@users.noreply.github.com>
2023-11-01 03:24:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
 <form action="check.php" method="POST">
   <div class="container mt-5 text-center" style="border-radius: 20px;background: rgb(182, 185, 191, 0.9)">
     <div class = "box">
-            <input type="text" name="username" class="form-control mt-2" placeholder="ชื่อผู้ใช้">
-            <input type="text" name="password" class="form-control mt-2" placeholder="รหัสผ่าน">
-            <button type="submit" class="btn btn-primary mt-2">เข้าสู่ระบบ</button>
+      <input type="text" name="username" class="form-control mt-2" placeholder="ชื่อผู้ใช้">
+      <input type="text" name="password" class="form-control mt-2" placeholder="รหัสผ่าน">
+      <button type="submit" class="btn btn-primary mt-2">เข้าสู่ระบบ</button>
     </div>
   </div>
 </form>
